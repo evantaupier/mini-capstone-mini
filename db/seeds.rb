95364f5d49e90991c0cae55919c5f0ec2ee72e7c
EvanTaupier <drummertitan@gmail.com>
2022-11-02 13:28:37 -0400

    items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "Plunger", price: 3, image_url: "https://www.bobhoeglerplumbing.com/wp-content/uploads/2019/02/GettyImages-173683465.jpg", description: "What goes down can always come up!")
+Product.create(name: "Wrench", price: 4, image_url: "https://m.media-amazon.com/images/I/614H3-Qgn-L._AC_SX355_.jpg", description: "If you can dodge a wrench, you can dodge a ball!")
+Product.create(name: "Dodgeball", price: 2, image_url: "https://media.istockphoto.com/vectors/dodgeball-illustration-vector-id478279115?k=20&m=478279115&s=612x612&w=0&h=HEfZArmayGXbmEB3z57K43bUo9iHF4vIvcnU0UBLHq8=", description: "If you can dodge a ball, you can dodge a wrench!")
